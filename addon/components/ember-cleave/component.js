0e9ab3a6b3ddf40838d22c51b825dc58f3929728
Robert Harężlak <harezlak.robert7@gmail.com>
2019-03-01 10:38:31 +0100

    Basic setup of component

--- addon/components/ember-cleave/component.js
+++ addon/components/ember-cleave/component.js
@@ -1,6 +1,12 @@
 import Component from '@ember/component';
 import layout from './template';
+import Cleave from 'cleave.js';
 
 export default Component.extend({
-  layout
+  layout,
+
+  didInsertElement() {
+    this._super(...arguments);
+    this.set('cleave', new Cleave(this.inputClass, this.options));
+  }
 });
