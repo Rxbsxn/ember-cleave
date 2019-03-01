c7314cc7597b8e034b690757c53a1d84329eae0b
Robert Harężlak <harezlak.robert7@gmail.com>
2019-03-01 13:20:11 +0100

    Add missing value property

--- addon/components/ember-cleave/component.js
+++ addon/components/ember-cleave/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import Cleave from 'cleave.js';
+import { isPresent } from '@ember/utils';
 
 export default Component.extend({
   tagName: 'input',
@@ -7,15 +8,26 @@ export default Component.extend({
 
   type: 'text',
 
+  keyUp() {
+    this.set('value', this.element.value)
+  },
+
   didInsertElement() {
     this._super(...arguments);
     this.set('cleave', new Cleave(`#${this.elementId}`, this.options));
+    this._setDefaultValue()
   },
 
   didReceiveAttrs() {
     this._super(...arguments);
+
     if (!this.options) {
       throw new Error('Missing options object');
     }
+  },
+
+  _setDefaultValue() {
+    if (isPresent(this.value))
+      this.element.value = this.value;
   }
 });
