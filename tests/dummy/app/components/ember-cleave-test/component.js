cd51fb4b3c3ddd09a16621357ec009fe0d11deea
Robert Harężlak <harezlak.robert7@gmail.com>
2019-03-01 11:41:52 +0100

    Add addon preview

---
+++ tests/dummy/app/components/ember-cleave-test/component.js
@@ -0,0 +1,61 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+export default Component.extend({
+  creditCardOptions: computed(function() {
+    return {
+      creditCard: true,
+      onCreditCardTypeChanged: this._cardNameLogger.bind(this)
+    }
+  }),
+
+
+  dateOptions: computed(function() {
+    return {
+      date: true,
+      datePattern: ['Y', 'm', 'd']
+    }
+  }),
+
+  timeOptions: computed(function() {
+    return {
+      time: true,
+      timePattern: ['h', 'm', 's']
+    }
+  }),
+
+  numeralOptions: computed(function() {
+    return {
+      numeral: true,
+      numeralThousandsGroupStyle: 'thousand'
+    }
+  }),
+
+  customOptions: computed(function() {
+    return {
+      blocks: [4, 3, 3, 4],
+      uppercase: true
+    }
+  }),
+
+  customOptionsWithDelimiter: computed(function() {
+    return {
+      delimiter: '.',
+      blocks: [4, 3, 3, 4],
+      uppercase: true
+    }
+  }),
+
+  customOptionsWithPrefix: computed(function() {
+    return {
+      delimiter: '-',
+      prefix: 'Ember',
+      blocks: [5, 3, 3, 4]
+    }
+  }),
+
+
+  _cardNameLogger(type) {
+    this.set('cardName', type);
+  }
+});
